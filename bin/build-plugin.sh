b150a9ab29bb420eaac68770b84c219f648cd5c6
chuckfairy <charlieabeling@gmail.com>
2024-06-11 08:07:15 -0500

    more plugin build cleaning

--- bin/build-plugin.sh
+++ bin/build-plugin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FOLDER=./dist/cornerstone-code-blocks;
+FOLDER=/tmp/cornerstone-code-blocks/cornerstone-code-blocks;
 
 # Setup folder
 mkdir -p $FOLDER;
@@ -16,9 +16,13 @@ cp -R ./ $FOLDER/;
 rm $FOLDER/package.json;
 rm $FOLDER/package-lock.json;
 rm $FOLDER/dist/.gitkeep
+rm $FOLDER/.gitignore
+rm $FOLDER/README.md
 rm -rf $FOLDER/node_modules;
 rm -rf $FOLDER/src;
 rm -rf $FOLDER/.git
+rm -rf $FOLDER/bin
+rm -rf $FOLDER/screenshots
 
 # Create zip
 zip -r ./dist/cornerstone-code-blocks.zip $FOLDER;
